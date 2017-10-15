fa0ac5efa92ed66ea07f2da2d84128a0244cdfbe
francois76 <fgognet@gmail.com>
2017-10-15 22:59:47 +0200

    update_sql

--- CuichetteLand.sql
+++ CuichetteLand.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb4
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
--- Client :  localhost:3306
--- Généré le :  Jeu 14 Septembre 2017 à 10:44
--- Version du serveur :  5.7.19-0ubuntu0.17.04.1
--- Version de PHP :  7.0.22-0ubuntu0.17.04.1
+-- Hôte : 127.0.0.1
+-- Généré le :  Dim 15 oct. 2017 à 22:56
+-- Version du serveur :  5.7.19-0ubuntu0.16.04.1
+-- Version de PHP :  7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,56 +25,82 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Admins`
+-- Structure de la table `images`
 --
 
-CREATE TABLE `Admins` (
-  `Identifiants` text NOT NULL,
-  `Mot_de_passe` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `id_produit` int(11) NOT NULL,
+  `nom_image` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `images`
+--
+
+INSERT INTO `images` (`id`, `id_produit`, `nom_image`) VALUES
+(1, 1, '500x_star_trek_spork.jpg'),
+(2, 2, '3216100.jpg'),
+(3, 3, '34620155.jpg'),
+(4, 4, '112854602_o.jpg'),
+(5, 5, 'IMG_0179.JPG'),
+(6, 6, 'IMGP8609.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Produits`
+-- Structure de la table `produits`
 --
 
-CREATE TABLE `Produits` (
-  `Identifiant` int(11) NOT NULL,
-  `Nom` text NOT NULL,
-  `Images` text NOT NULL,
-  `Prix` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Informations sur les produits';
-
--- --------------------------------------------------------
+CREATE TABLE `produits` (
+  `id` int(11) NOT NULL,
+  `Nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `Prix` double NOT NULL,
+  `Categorie` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Structure de la table `Utilisateurs`
+-- Déchargement des données de la table `produits`
 --
 
-CREATE TABLE `Utilisateurs` (
-  `Nom` text NOT NULL,
-  `Prenom` text NOT NULL,
-  `Age` int(11) NOT NULL,
-  `Mail` text NOT NULL,
-  `Numero_telephone` text NOT NULL,
-  `Identifiant` text NOT NULL,
-  `Mot_de_passe` text NOT NULL,
-  `Numero_CB` int(11) NOT NULL,
-  `Cryptogramme_CB` int(11) NOT NULL,
-  `Date_CB` date NOT NULL,
-  `Nom_CB` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Utilisateurs de CuichetteLand';
+INSERT INTO `produits` (`id`, `Nom`, `Prix`, `Categorie`) VALUES
+(1, 'Grosse Cuichette', 9.99, 'Grande'),
+(2, 'Mega cuichette', 10.25, 'Grande'),
+(3, 'Giga cuichette', 15.25, 'Grande'),
+(4, 'Mini cuichette', 4.1, 'Petite'),
+(5, 'Micro cuichette', 2.25, 'Petite'),
+(6, 'Nano cuichette', 1.8, 'Petite');
 
 --
--- Index pour les tables exportées
+-- Index pour les tables déchargées
 --
 
 --
--- Index pour la table `Produits`
+-- Index pour la table `images`
 --
-ALTER TABLE `Produits`
-  ADD PRIMARY KEY (`Identifiant`);
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `produits`
+--
+ALTER TABLE `produits`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT pour la table `produits`
+--
+ALTER TABLE `produits`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
